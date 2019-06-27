2f9cfaa2af97eaa09a0eb65047844abefee31df6
Shubham Bhadani <shubham.bhadani07@gmail.com>
2019-06-27 12:47:56 +0530

    Add files via upload

---
+++ src/api/GifApi.js
@@ -0,0 +1,9 @@
+import GphApiClient from './node_modules/giphy-js-sdk-core';
+
+const client = GphApiClient('MTi47j0kagCljkuKvVR9zIrph7FaioUY');
+
+const searchGifs = async (q = "", limit = 12, offset = 0) => {
+    return await client.search('gifs', {q, limit, offset});
+};
+
+export {searchGifs};
